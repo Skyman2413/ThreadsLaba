c20d4fa677404b5d8d8c3ea6febf9a978d02cea6
skyman2413 <stepan26062001@gmail.com>
2023-04-05 00:33:04 +0300

    added multithread

---
+++ api_server_multi_thread.py
@@ -0,0 +1,128 @@
+import datetime
+import json
+import os
+from pathlib import Path
+from queue import Queue
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+from threading import Thread, Lock
+
+from worker import Worker, monitoring_list, queue_to_produce
+
+max_id = 0
+sample_queue_element = {
+    "org_name": "sampleName",
+    "date": "21.04.2023",
+    "product_list": [["ProductA", 1, 4059], ["ProductB", 2, 930], ["ProductC", 3, 3444]],
+    "state": "Done Successfully/Error",
+    "worker": "worker1",
+    "id": 1
+}
+
+
+class MyHHTPRequestHandler(BaseHTTPRequestHandler):
+    # Обработчик GET запроса. В нашей программе таких будет 2 - просмотреть всю очередь
+    # и получить сформированный файл
+    def do_GET(self):
+        if self.path == "/get_file":
+            # Задание для студентов - реализовать обработку получения файла из готовой очереди
+            status_code = 200
+            status_info = "OK"
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            json_body = {}
+            if self.headers["Content-Type"].lower() not in ("text/json", "application/json"):
+                status_code = 405
+                status_info = "Method Not Allowed"
+            try:
+                json_body = json.loads(body)
+
+            except Exception as e:
+                print(e)
+                status_code = 400
+                status_info = "Bad Request"
+            pass
+            if status_code == 200:
+                if not os.path.exists(Path(os.getcwd(), "files_to_send", f"{json_body['id']}.xlsx")):
+                    status_code = 404
+                    status_info = "Not found"
+            self.send_response(status_code, status_info)
+            self.send_header('Content-type', 'application/xlsx')
+            self.send_header('Content-Disposition', f'attachment; filename="{json_body["id"]}.xlsx"')
+            self.end_headers()
+            if status_code == 200:
+                with open(Path(os.getcwd(), "files_to_send", f"{json_body['id']}.xlsx"), 'rb') as file:
+                    self.wfile.write(file.read())
+        if self.path == "/queue_info":
+            pass
+            status_code = 200
+            status_info = "OK"
+            response_body = json.dumps(monitoring_list)
+            self.send_response(status_code, status_info)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(response_body, "utf-8"))
+
+    # Обработчик POST запроса для добавления элемента в очередь
+    def do_POST(self):
+        global max_id
+        status_code = 200
+        status_info = "OK"
+
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        response_body = ""
+        if self.headers["Content-Type"].lower() not in ("text/json", "application/json"):
+            status_code = 405
+            status_info = "Method Not Allowed"
+        try:
+            json_body = json.loads(body)
+            max_id = max_id + 1
+            id = max_id
+            new_element = {"org_name": json_body["org_name"], "date": json_body["date"],
+                           "product_list": json_body["product_list"], "state": "new", "worker": "",
+                           "id": id}
+            queue_to_produce.put(new_element)
+            response_body = {"id": new_element["id"]}
+            response_body = json.dumps(response_body)
+
+            lock = Lock()
+            # блокируем словарь для выполнения не атомарных операций
+            with lock:
+                monitoring_list[id] = new_element
+                monitoring_list[id]["created_at"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+
+            # По-хорошему нужно проверить каждый на корректность. Сейчас может возникнуть только KeyError
+
+        except Exception as e:
+            print(e)
+            status_code = 400
+            status_info = "Bad Request"
+            new_element = {}
+        if len(new_element) == 0:
+            response_body = "{}"
+        print("Получен элемент\r\n")
+        print(str(new_element) + "\r\n")
+
+        self.send_response(status_code, status_info)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(response_body, "utf-8"))
+        pass
+
+
+if __name__ == "__main__":
+    worker_count = 8
+    threads = []
+    httpd = ThreadingHTTPServer(("localhost", 8080), MyHHTPRequestHandler)
+
+    t = Thread(target=httpd.serve_forever)
+
+    for i in range(1, worker_count+1):
+        worker = Worker(name=f"Worker{i}")
+        threads.append(worker)
+    threads.append(t)
+
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
---
+++ worker.py
@@ -0,0 +1,79 @@
+import datetime
+import os
+import random
+import shutil
+import time
+from pathlib import Path
+from queue import Queue
+from threading import Thread, Lock
+
+
+import openpyxl
+queue_to_produce = Queue()
+# данный лист нужны только для мониторинга, по-хорошему он не нужен, лишний расход памяти
+monitoring_list = {}
+
+class Worker(Thread):
+    name: str
+
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+        print(f"объект {self.name} создан")
+
+    def run(self):
+        print(f"запущен поток {self.name}")
+        while True:
+            item = queue_to_produce.get()
+            print(f"Начало работы над элементом с id {item['id']} by {self.name} "
+                  f" в {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+            item["worker"] = f"Worker{self.name}"
+            item["state"] = "in_progress"
+            lock = Lock()
+            # блокируем словарь для выполнения не атомарных операций
+            with lock:
+                monitoring_list[item["id"]]["state"] = "in progress"
+                monitoring_list[item["id"]]["worker"] = self.name
+                monitoring_list[item["id"]]["start_date"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+            try:
+                self.fill_excel(item)
+                # блокируем словарь для выполнения не атомарных операций
+                with lock:
+                    monitoring_list[item["id"]]["state"] = "Done Successfully"
+            except Exception as e:
+                print(e)
+                # блокируем словарь для выполнения не атомарных операций
+                with lock:
+                    monitoring_list[item["id"]]["state"] = "Error"
+            # блокируем словарь для выполнения не атомарных операций
+            with lock:
+                monitoring_list[item["id"]]["end_date"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+            queue_to_produce.task_done()
+            print(f"Конец работы над элементом с id {item['id']} by {self.name}"
+                  f" в {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+
+    def fill_excel(self, item: {}):
+        # такой код будет работать вне зависимости от ОС
+        path_to_excel = Path(os.getcwd(), "files_to_send", f"{item['id']}.xlsx")
+        path_to_template = Path(os.getcwd(), "Товарный-чек.xlsx")
+        shutil.copyfile(path_to_template, path_to_excel)
+        wb = openpyxl.load_workbook(path_to_excel)
+        ws = wb.active
+        ws["A1"].value = item["org_name"]
+        title_cell = ws["A4"].value
+        title_cell = title_cell.replace("id", str(item["id"])).replace("date", item["date"])
+        ws["A4"].value = title_cell
+        all_sum = 0
+        i = 6
+        for product in item["product_list"]:
+            ws[f"A{i}"].value = i - 5
+            ws[f"B{i}"].value = product[0]
+            ws[f"H{i}"].value = product[1]
+            ws[f"J{i}"].value = product[2]
+            ws[f"M{i}"].value = product[1] * product[2]
+            all_sum += product[1] * product[2]
+            i += 1
+        ws["M16"].value = all_sum
+        wb.save(path_to_excel)
+        wb.close()
+        time.sleep(random.randint(5, 25))
