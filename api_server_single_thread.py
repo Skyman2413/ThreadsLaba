343f92887f1ee8bf7542e746762e0abd44e6c9fc
skyman2413 <stepan26062001@gmail.com>
2023-04-06 15:29:16 +0300

    refactor

--- api_server_single_thread.py
+++ api_server_single_thread.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread, Lock
 
+import worker
 from worker import Worker, monitoring_list, queue_to_consume
 
 max_id = 0
@@ -81,7 +82,8 @@ class MyHHTPRequestHandler(BaseHTTPRequestHandler):
             new_element = {"org_name": json_body["org_name"], "date": json_body["date"],
                            "product_list": json_body["product_list"], "state": "new", "worker": "",
                            "id": id}
-            queue_to_consume.put(new_element)
+            # для очереди
+            # queue_to_consume.put(new_element)
             response_body = {"id": new_element["id"]}
             response_body = json.dumps(response_body)
 
@@ -91,6 +93,7 @@ class MyHHTPRequestHandler(BaseHTTPRequestHandler):
                 monitoring_list[id] = new_element
                 monitoring_list[id]["created_at"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
             time.sleep(2)
+
             # По-хорошему нужно проверить каждый на корректность. Сейчас может возникнуть только KeyError
 
         except Exception as e:
@@ -107,13 +110,14 @@ class MyHHTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(response_body, "utf-8"))
-        pass
-
+        worker.worker_without_thread(item=new_element)
 
 if __name__ == "__main__":
-    worker_count = 8
     threads = []
     httpd = HTTPServer(("localhost", 8080), MyHHTPRequestHandler)
+    httpd.serve_forever()
+    # для очереди
+    '''worker_count = 8
     t = Thread(target=httpd.serve_forever)
 
     for i in range(1, worker_count+1):
@@ -124,4 +128,4 @@ if __name__ == "__main__":
     for thread in threads:
         thread.start()
     for thread in threads:
-        thread.join()
+        thread.join()'''
--- worker.py
+++ worker.py
@@ -7,11 +7,53 @@ from pathlib import Path
 from queue import Queue
 from threading import Thread, Lock
 
-
 import openpyxl
+
 queue_to_consume = Queue()
 # данный словарь нужны только для мониторинга, по-хорошему он не нужен, лишний расход памяти
 monitoring_list = {}
+
+
+def worker_without_thread(item: {}):
+    print(f"Начало работы над элементом с id {item['id']}"
+          f" в {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+    item["state"] = "in_progress"
+    lock = Lock()
+    # блокируем словарь для выполнения не атомарных операций
+    monitoring_list[item["id"]]["state"] = "in progress"
+    monitoring_list[item["id"]]["start_date"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+    try:
+        path_to_excel = Path(os.getcwd(), "files_to_send", f"{item['id']}.xlsx")
+        path_to_template = Path(os.getcwd(), "Товарный-чек.xlsx")
+        shutil.copyfile(path_to_template, path_to_excel)
+        wb = openpyxl.load_workbook(path_to_excel)
+        ws = wb.active
+        ws["A1"].value = item["org_name"]
+        title_cell = ws["A4"].value
+        title_cell = title_cell.replace("id", str(item["id"])).replace("date", item["date"])
+        ws["A4"].value = title_cell
+        all_sum = 0
+        i = 6
+        for product in item["product_list"]:
+            ws[f"A{i}"].value = i - 5
+            ws[f"B{i}"].value = product[0]
+            ws[f"H{i}"].value = product[1]
+            ws[f"J{i}"].value = product[2]
+            ws[f"M{i}"].value = product[1] * product[2]
+            all_sum += product[1] * product[2]
+            i += 1
+        ws["M16"].value = all_sum
+        wb.save(path_to_excel)
+        wb.close()
+        time.sleep(random.randint(5, 25))
+        monitoring_list[item["id"]]["state"] = "Done Successfully"
+    except Exception as e:
+        print(e)
+        monitoring_list[item["id"]]["state"] = "Error"
+    monitoring_list[item["id"]]["end_date"] = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+    print(f"Конец работы над элементом с id {item['id']}"
+          f" в {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+
 
 class Worker(Thread):
     name: str
